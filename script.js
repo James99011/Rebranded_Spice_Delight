eeda55df1170fa92bf4a5dabe65da65df661c990
James99011 <jegedejames71@gmail.com>
2025-09-02 08:25:01 -0600

    Faith

--- script.js
+++ script.js
@@ -140,7 +140,7 @@ window.addEventListener('load', updateScrollBtn);
 
 
 
-
+// This prevents quick double taps from zooming or shifting the screen and safari can't override it
 let lastTouch = 0;
 document.addEventListener('touchend', function(event) {
   const now = Date.now();
