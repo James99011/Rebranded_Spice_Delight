5270687c6286a2ec5b03ba3de0ac7c4e108a7752
James99011 <jegedejames71@gmail.com>
2025-09-16 21:55:52 -0600

    Up

---
+++ shop/shop.js
@@ -0,0 +1,37 @@
+const navToggle = document.getElementById('toggleSymbol');
+const mobileMenu = document.getElementById('mobileMenu');
+
+navToggle.addEventListener('click', () => {
+  mobileMenu.classList.toggle('show');
+
+  // Toggle between + and ×
+  if (navToggle.textContent === '+') {
+    navToggle.textContent = '×';
+  } else {
+    navToggle.textContent = '+';
+  }
+});
+
+
+  
+let cartCount = 0;
+let menuOpen = false;
+
+const toggleSymbol = document.getElementById('toggleSymbol');
+const cartCountSpan = document.getElementById('cartCount');
+const navTogglee = document.getElementById('navToggle');
+
+function updateCartCount() {
+  cartCountSpan.textContent = `(${cartCount})`;
+}
+
+function addToCart() {
+  cartCount++;
+  updateCartCount();
+}
+
+
+// Initialize count on load
+updateCartCount();
+
+
